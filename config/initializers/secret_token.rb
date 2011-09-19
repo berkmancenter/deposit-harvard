7dca009eaf199b9e824b4efd2254de103ea0afca
Srdjan Pejic <spejic@gmail.com>
2011-09-19 18:31:42 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepositHarvard::Application.config.secret_token = '0f3f4170495de9619b14116ced6736319e5f73c807f819b80ebc0fcd3f356289ba1b0e61d974521e78ef46164925fc83d06cb7af776df6b7dd9a9f8494b283e6'
